197d9bbdc40eaee01966f8504824f2d21c8a1669
Alex <alex.damour91@gmail.com>
2022-08-16 15:42:34 +0200

    Added addresses of deployed smart contracts

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
-export const nftmarketaddress = "ADDRESS-MARKETPLACE";
-export const nftcollectionaddress = "ADDRESS-NFT-COLLECTION";
+export const nftmarketaddress = "0x1D05e0E0AF984F24422E915c7B017312d1b229C3";
+export const nftcollectionaddress =
+  "0x9Fec3ae7f43A324cA68EB513CbE75210605e3422";
